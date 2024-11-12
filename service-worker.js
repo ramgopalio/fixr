8c0e13cc804a618bbc50ecdb4720ec79829b878a
ramgopalio <ramgopal.metta@outlook.com>
2024-11-12 23:36:17 +0530

    pwa

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'fixr-company-cache-v1';
+const urlsToCache = [
+  '/',
+  'css/main.css',
+  'js/main.js',
+  '/index.html',
+  // Add other assets you want cached
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }).catch(() => caches.match('/offline.html'))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
