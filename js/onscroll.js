e20a903a85074c8147a8e21f5787623e1323a4cb
ramgopalio <ramgopal.metta@outlook.com>
2024-11-14 02:26:01 +0530

    onscroll-js

---
+++ js/onscroll.js
@@ -0,0 +1,14 @@
+function onScroll (evt) {
+
+    // Store the scroll value for laterz.
+    lastScrollY = window.scrollY;
+
+    // Prevent multiple rAF callbacks.
+    if (scheduledAnimationFrame)
+    return;
+
+    scheduledAnimationFrame = true;
+    requestAnimationFrame(readAndUpdatePage);
+}
+
+window.addEventListener('scroll', onScroll);
